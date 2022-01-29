88fe10d126313a48bf65133752815eaf43a521c2
bluepark-sk <blue4park@gmail.com>
2022-01-29 19:32:50 +0900

    CH7 합성곱 신경망 (CNN)

---
+++ CH7. 합성곱 신경망 (CNN)/CNN.py
@@ -0,0 +1,126 @@
+# 7.5 CNN 구현하기
+from collections import OrderedDict
+import numpy as np
+from convolution_pooling import Convolution, Pooling
+from common.functions import cross_entropy_error
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+    
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+        return out
+    
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        return dx
+    
+class Relu:
+    def __init__(self):
+        self.mask = None
+    
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+    
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+class SoftmaxWithLoss:
+    def __init__(self) -> None:
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+    
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        return dx
+
+class SimpleConvNet:
+    def __init__(self,
+                 input_dim=(1, 28, 28),
+                 conv_param={'filter_num':30, 'filter_size':5, 'pad':0, 'stride':1},
+                 hidden_size=100, output_size=10, weight_init_std=0.01):
+        filter_num = conv_param['filter_num']
+        filter_size = conv_param['filter_size']
+        filter_pad = conv_param['pad']
+        filter_stride = conv_param['stride']
+        input_size = input_dim[1]
+        conv_output_size = (input_size - filter_size + 2*filter_pad) / filter_stride + 1
+        pool_output_size = int(filter_num * (conv_output_size/2) * (conv_output_size/2))
+
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
+        self.params['b1'] = np.zeros(filter_num)
+        self.params['W2'] = weight_init_std * np.random.randn(pool_output_size, hidden_size)
+        self.params['b2'] = np.zeros(hidden_size)
+        self.params['W3'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b3'] = np.zeros(output_size)
+
+        self.layers = OrderedDict()
+        self.layers['Conv1'] = Convolution(self.params['W1'], self.params['b1'], conv_param['stride'], conv_param['pad'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+        self.layers['Relu2'] = Relu()
+        self.layers['Affine3'] = Affine(self.params['W3'], self.params['b3'])
+        self.last_layer = SoftmaxWithLoss()
+
+        def predict(self, x):
+            for layer in self.layers.values():
+                x = layer.forward(x)
+            return x
+        
+        def loss(self, x, t):
+            y = self.predict(x)
+            return self.last_layer.forward(y, t)
+        
+        def gradient(self, x, t):
+            # 순전파
+            self.loss(x, t)
+
+            # 역전파
+            dout = 1
+            dout = self.last_layer.backward(dout)
+
+            layers = list(self.layers.values())
+            layers.reverse()
+            for layer in layers:
+                dout = layer.backward(dout)
+            
+            # 결과 저장
+            grads = {}
+            grads['W1'] = self.layers['Conv1'].dW
+            grads['b1'] = self.layers['Conv1'].db
+            grads['W2'] = self.layers['Affine1'].dW
+            grads['b2'] = self.layers['Affine1'].db
+            grads['W3'] = self.layers['Affine2'].dW
+            grads['b3'] = self.layers['Affine2'].db
+
+            return grads
